6f9c8779fd72ba09f4ff43f75385a43d419f5e4e
Gustavo Sanchez <antero105@gmail.com>
2021-05-11 22:22:08 -0400

    Adding test

---
+++ src/__test__/unit/postman.test.ts
@@ -0,0 +1,88 @@
+
+import {Postman} from '../../postman'
+import {PostmanController} from "../../interfaces/postmanController";
+import * as fs from "fs";
+
+jest.mock('fs');
+
+const mockWriteSync = jest.spyOn(fs as any, 'writeFile').mockImplementation();
+const obj = [{
+    name: 'Test',
+    description: 'Description',
+    routes: [{
+        name: 'test endpoint',
+        url: 'test',
+        method: 'GET',
+        description: 'Some description',
+        body: 'body'
+    }]
+} as PostmanController];
+describe('Postman Unit test', () =>{
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+    it('Generate controllers', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        await postman.run('../', './');
+        expect(mockWriteSync).toHaveBeenCalledTimes(1);
+        expect(generateControllers).toHaveBeenCalledTimes(1);
+    });
+    it('Generate controllers with routes without body', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        await postman.run('../', './');
+        expect(mockWriteSync).toHaveBeenCalledTimes(1);
+        expect(generateControllers).toHaveBeenCalledTimes(1);
+    })
+
+    it('Generate controllers with routes with body', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        await postman.run('../', './');
+        expect(mockWriteSync).toHaveBeenCalledTimes(1);
+        expect(generateControllers).toHaveBeenCalledTimes(1);
+    })
+
+    it('Generate controllers with verbose', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        postman.verbose = true;
+        mockWriteSync.mockImplementation((f,d,callback: () => void) => {
+            callback()
+        });
+        await postman.run('../', './');
+        expect(mockWriteSync).toHaveBeenCalledTimes(1);
+        expect(generateControllers).toHaveBeenCalledTimes(1);
+    })
+
+    it('Generate controllers with exception', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        try {
+            await postman.run('../', './');
+        } catch (e) {
+            expect(generateControllers).toHaveBeenCalledTimes(1);
+            expect(mockWriteSync).toHaveBeenCalledTimes(0);
+            expect(e).toBe('error');
+        }
+
+    })
+
+    it('Generate controllers with exception on readFileSync', async () => {
+        const postman = new Postman('Test', 'https://test.com');
+        const generateControllers = jest.spyOn(Postman.prototype as any, 'generateControllers').mockImplementation(() => obj);
+        mockWriteSync.mockImplementation((f,d,callback: (error:string) => void) => {
+            callback('error')
+        });
+        try {
+            await postman.run('../', './');
+        } catch (e) {
+            expect(generateControllers).toHaveBeenCalledTimes(1);
+            expect(mockWriteSync).toHaveBeenCalledTimes(1);
+            expect(e).toBe('error');
+        }
+
+    })
+
+})
